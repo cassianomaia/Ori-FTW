710094784e720b5a4358487dd229ac6423bc9940
juumilani <settieyeah@gmail.com>
2017-10-24 15:54:25 -0200

    arquivo .h do file manager

---
+++ file.h
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define tamReg 62
+#define tamBloco 512
+#define tamCod 4
+#define tamDesc 50
+#define tamAno 4
+#define tamValor 4
+
+
+// struct que define registros de 62 bytes para um arquivo de catalogo de violinos
+typedef struct {
+	
+	int cod[4];
+	char desc[50];
+	int ano[4];
+	float valor[4];		// 4 + 50 + 4 + 4 = 62 bytes
+	
+} reg;
+
+
+// struct que define os blocos do programa, com tamanho fixo de 512 bytes
+typedef struct {
+	
+	char header[2];	//CATITO TA SOBRANDO 14 BYTES
+	reg index[8];	//quantidade de registros de 62 bytes que cabem no bloco
+	
+} bloco; 
+
+
+// operações dos registros
+int insereReg(reg, char*);			//parametros: reg e filepath
+int removeReg(int*, char*);			//codigo (key) e filepath
+void readReg(reg*);
+void writeReg(reg);
+int procuraReg(reg*, int*, char*)	//reg, key e filepath
+
+//operação do bloco
+bloco* criaBloco();
+
+//operações do arquivo
+int criaFile(char*);
+int compactaFile(char*);
